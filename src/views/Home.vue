0fad466a6fe652faf1fe9d5ee46f9c37d454a809
869288142 <869288142@qq.com>
2019-01-23 16:47:34 +0800

    实现login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input class="username" v-model="username" type="text" name="" id="" placeholder="用户名">
+    <input class="password" v-model="password" type="text" name="" id="" placeholder="密码">
+    <input class="showLogin" v-model="isLogin" type="text" name="" id="">
+    <button @click="login">登录</button>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from 'axios'
+axios.defaults.proxy = false || {
+  host: '127.0.0.1',
+  port: 7001
+}
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data (){
+    return {
+      username: '',
+      password: '',
+      isLogin: false
+    }
+  },
+  methods: {
+    async login() {
+      ({data:this.isLogin} = await axios.post('/login',{username: this.username, password: this.password}))
+      if(this.isLogin){
+        this.$router.push({name:'about'})
+      }
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+  .home{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .username{
+    width: 300px;
+  }
+  .password{
+    width: 300px;
+  }
+</style>
